f3b3e75c3e64e4532b975be01b25f34e8d248536
Shubham <shubham@ajency.in>
2019-10-31 11:33:38 +0530

    Fetch data via server side
    API calls from javascript have been removed

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <html>
 	<head>
 	<script type="text/javascript"	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script type="text/javascript"	src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js"></script>
-		
+	
         
         
           <link rel="alternate" media="only screen and (max-width: 640px)" href="https://m.weddingz.in/blog/">
@@ -61,36 +60,6 @@
 .fb_dialog{background:rgba(82, 82, 82, .7);position:absolute;top:-10000px;z-index:10001}.fb_dialog_advanced{border-radius:8px;padding:10px}.fb_dialog_content{background:#fff;color:#373737}.fb_dialog_close_icon{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/IE9JII6Z1Ys.png) no-repeat scroll 0 0 transparent;cursor:pointer;display:block;height:15px;position:absolute;right:18px;top:17px;width:15px}.fb_dialog_mobile .fb_dialog_close_icon{left:5px;right:auto;top:5px}.fb_dialog_padding{background-color:transparent;position:absolute;width:1px;z-index:-1}.fb_dialog_close_icon:hover{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/IE9JII6Z1Ys.png) no-repeat scroll 0 -15px transparent}.fb_dialog_close_icon:active{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/IE9JII6Z1Ys.png) no-repeat scroll 0 -30px transparent}.fb_dialog_iframe{line-height:0}.fb_dialog_content .dialog_title{background:#6d84b4;border:1px solid #365899;color:#fff;font-size:14px;font-weight:bold;margin:0}.fb_dialog_content .dialog_title>span{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/yd/r/Cou7n-nqK52.gif) no-repeat 5px 50%;float:left;padding:5px 0 7px 26px}body.fb_hidden{height:100%;left:0;margin:0;overflow:visible;position:absolute;top:-10000px;transform:none;width:100%}.fb_dialog.fb_dialog_mobile.loading{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/ya/r/3rhSv5V8j3o.gif) white no-repeat 50% 50%;min-height:100%;min-width:100%;overflow:hidden;position:absolute;top:0;z-index:10001}.fb_dialog.fb_dialog_mobile.loading.centered{background:none;height:auto;min-height:initial;min-width:initial;width:auto}.fb_dialog.fb_dialog_mobile.loading.centered #fb_dialog_loader_spinner{width:100%}.fb_dialog.fb_dialog_mobile.loading.centered .fb_dialog_content{background:none}.loading.centered #fb_dialog_loader_close{clear:both;color:#fff;display:block;font-size:18px;padding-top:20px}#fb-root #fb_dialog_ipad_overlay{background:rgba(0, 0, 0, .4);bottom:0;left:0;min-height:100%;position:absolute;right:0;top:0;width:100%;z-index:10000}#fb-root #fb_dialog_ipad_overlay.hidden{display:none}.fb_dialog.fb_dialog_mobile.loading iframe{visibility:hidden}.fb_dialog_mobile .fb_dialog_iframe{position:sticky;top:0}.fb_dialog_content .dialog_header{background:linear-gradient(from(#738aba), to(#2c4987));border-bottom:1px solid;border-color:#043b87;box-shadow:white 0 1px 1px -1px inset;color:#fff;font:bold 14px Helvetica, sans-serif;text-overflow:ellipsis;text-shadow:rgba(0, 30, 84, .296875) 0 -1px 0;vertical-align:middle;white-space:nowrap}.fb_dialog_content .dialog_header table{height:43px;width:100%}.fb_dialog_content .dialog_header td.header_left{font-size:12px;padding-left:5px;vertical-align:middle;width:60px}.fb_dialog_content .dialog_header td.header_right{font-size:12px;padding-right:5px;vertical-align:middle;width:60px}.fb_dialog_content .touchable_button{background:linear-gradient(from(#4267B2), to(#2a4887));background-clip:padding-box;border:1px solid #29487d;border-radius:3px;display:inline-block;line-height:18px;margin-top:3px;max-width:85px;padding:4px 12px;position:relative}.fb_dialog_content .dialog_header .touchable_button input{background:none;border:none;color:#fff;font:bold 12px Helvetica, sans-serif;margin:2px -12px;padding:2px 6px 3px 6px;text-shadow:rgba(0, 30, 84, .296875) 0 -1px 0}.fb_dialog_content .dialog_header .header_center{color:#fff;font-size:16px;font-weight:bold;line-height:18px;text-align:center;vertical-align:middle}.fb_dialog_content .dialog_content{background:url(https://static.xx.fbcdn.net/rsrc.php/v3/y9/r/jKEcVPZFk-2.gif) no-repeat 50% 50%;border:1px solid #4a4a4a;border-bottom:0;border-top:0;height:150px}.fb_dialog_content .dialog_footer{background:#f5f6f7;border:1px solid #4a4a4a;border-top-color:#ccc;height:40px}#fb_dialog_loader_close{float:left}.fb_dialog.fb_dialog_mobile .fb_dialog_close_button{text-shadow:rgba(0, 30, 84, .296875) 0 -1px 0}.fb_dialog.fb_dialog_mobile .fb_dialog_close_icon{visibility:hidden}#fb_dialog_loader_spinner{animation:rotateSpinner 1.2s linear infinite;background-color:transparent;background-image:url(https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/t-wz8gw1xG1.png);background-position:50% 50%;background-repeat:no-repeat;height:24px;width:24px}@keyframes rotateSpinner{0%{transform:rotate(0deg)}100%{transform:rotate(360deg)}}
 .fb_iframe_widget{display:inline-block;position:relative}.fb_iframe_widget span{display:inline-block;position:relative;text-align:justify}.fb_iframe_widget iframe{position:absolute}.fb_iframe_widget_fluid_desktop,.fb_iframe_widget_fluid_desktop span,.fb_iframe_widget_fluid_desktop iframe{max-width:100%}.fb_iframe_widget_fluid_desktop iframe{min-width:220px;position:relative}.fb_iframe_widget_lift{z-index:1}.fb_iframe_widget_fluid{display:inline}.fb_iframe_widget_fluid span{width:100%}</style>
 
- <script type="text/javascript">
-	$(document).ready(function() {
-		url = window.location.href.split("?");
-		if(url.length == 2){
-			ajaxurl = 'http://ethicalseosolutions.net/newweddingz/wp-json/wp/v2/posts/'+url[1]+'?_embed';
-			$.ajax({url: ajaxurl, success: function(element){
-
-				var author = element._embedded.author[0].name;
-				var date = moment(element.date).format('MMM DD, YYYY');
-				var auth = "By "+author+" | "+date;
-				var fimg = element._embedded['wp:featuredmedia'][0].source_url;
-				var img = '<img src="'+fimg+'" >';
-				
-				var link = $("<a>");
-					link.attr("href",element.link);
-					link.attr("target","_blank");
-					link.attr("title",element.title.rendered);
-					link.text(element.title.rendered);
-					link.addClass("link");
-				
-				$('.content-wrap .item-title').html(element.title.rendered);
-				$('.content-wrap .article-meta').html(auth);
-				$('.content-wrap .article-content p').append(img);
-				$('.content-wrap .article-content p').append(element.content.rendered);
-	
-			}});	
-		}	
-	});
-
- </script>
 </head>
  <body>
  <header class="desktop-header" style="left: 0px;">
@@ -676,17 +645,49 @@
 	</div>
 	
 </header>
+	<?php
+		$title = '';
+		$author = '';
+		$image = '';
+		$content = '';
+		$reqUrl = explode("?",$_SERVER['REQUEST_URI']);
+		if(count($reqUrl) == 2){
+			$url = "http://ethicalseosolutions.net/newweddingz/wp-json/wp/v2/posts/".$reqUrl[1]."?_embed";
+			$curlSession = curl_init();
+			curl_setopt($curlSession, CURLOPT_URL, $url);
+			curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+			curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+			$jsonData = json_decode(curl_exec($curlSession));
+			
+			if(array_key_exists('id', $jsonData)){
+				$fi = 'wp:featuredmedia';
+				$title = $jsonData->title->rendered;
+				if(array_key_exists($fi, $jsonData->_embedded)){
+					$image = $jsonData->_embedded->$fi[0]->source_url;
+				}
+				$content = $jsonData->content->rendered;
+				$auth = $jsonData->_embedded->author[0]->name;
+				$dateObj = new DateTime($jsonData->date);
+    			$date = $dateObj->format('M d, Y');
+				$author = "By ".$auth." | ".$date;
+			}
+			curl_close($curlSession);
+		}
+	?>
 	<div class="container content-wrap">
 		<div class="row">
 			<div class="col-md-9 cover-white">
 			<h1 class="item-title mBtm-20">
-				
+				<?php echo $title; ?>
 			</h1>
 			<div class="article-meta text-center">
-
+				<?php echo $author; ?>
 			</div>
 			<div class="article-content">
-				<p class="text-center"></p>
+				<p class="text-center">
+					<img src="<?php echo $image; ?>" >
+					<?php echo $content; ?>
+				</p>
 			</div>
 			</div>
 
